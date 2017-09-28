b3fbca1a184b602ff3afdfbe7ea112faf4adb45d
Peter <Peter92@users.noreply.github.com>
2017-09-29 00:53:27 +0100

    Add session and login decorator

--- core/session.py
+++ core/session.py
@@ -1,19 +1,17 @@
-from flask import session
+from __future__ import absolute_import
+from flask import session, redirect, url_for, flash, request
+from functools import wraps
 import cPickle
-import hashlib
 import uuid
 import time
 
+from core.hash import quick_hash
+import core.tracking as tracking
+
 
 SESSION_TIMEOUT = 3600
-
-
-def quick_hash(x):
-    hash = hashlib.sha1()
-    hash.update(str(x))
-    return hash.hexdigest()
     
-    
+
 class SessionManager(object):
     def __init__(self, db_connection):
         self.sql = db_connection.sql
@@ -24,10 +22,9 @@ class SessionManager(object):
             hash = quick_hash(session_id)
             session_data = self.sql('SELECT data_pickle, last_activity FROM temporary_storage WHERE id = %s', hash)
             if session_data and session_data[0][1] > time.time() - SESSION_TIMEOUT:
-                session['sid'] = session_id
                 self.hash = hash
                 self.data = cPickle.loads(session_data[0][0])
-                self.new_id = False
+                self._new_id = False
                 return self
             else:
                 raise KeyError
@@ -44,6 +41,21 @@ class SessionManager(object):
     def get(self, item, default):
         return self.data.get(item, default)
     
+    def keys(self):
+        return self.data.keys()
+    
+    def values(self):
+        return self.data.values()
+        
+    def pop(self, item):
+        return self.data.pop(item)
+        
+    def iteritems(self):
+        return self.data.iteritems()
+        
+    def items(self):
+        return self.data.items()
+    
     def new(self):
         self.regenerate()
         self.data = {}
@@ -55,12 +67,26 @@ class SessionManager(object):
             if not self.sql('SELECT count(*) FROM temporary_storage WHERE id = %s', hash):
                 session['sid'] = session_id
                 self.hash = hash
-                self.new_id = True
+                self._new_id = True
                 return session_id
     
     def __exit__(self, *args):
         data = cPickle.dumps(self.data)
-        if self.new_id:
+        if self._new_id:
             self.sql('INSERT INTO temporary_storage (id, data_pickle, last_activity) VALUES(%s, %s, UNIX_TIMESTAMP(NOW()))', self.hash, data)
         else:
             self.sql('UPDATE temporary_storage SET data_pickle = %s, last_activity = UNIX_TIMESTAMP(NOW()) WHERE id = %s', data, self.hash)
+            
+
+def session_start(mysql, require_login=False):
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            with SessionManager(mysql) as session:
+                if require_login:
+                    if not session.get('account_id', 0):
+                        session['login_redirect'] = '{}?{}'.format(url_for(func.__name__, *args, **kwargs), request.query_string)
+                        return redirect(url_for('login'))
+                return func(session=session, *args, **kwargs)
+        return wrapper
+    return decorator
