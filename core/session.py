a6ef419f29842fd1a320b6f24f3627060e6cbb03
Peter <Peter92@users.noreply.github.com>
2017-09-29 14:29:07 +0100

    Track pages and compress session data

--- core/session.py
+++ core/session.py
@@ -1,16 +1,17 @@
 from __future__ import absolute_import
-from flask import session, redirect, url_for, flash, request
+from flask import session, redirect, url_for, abort, request
 from functools import wraps
 import cPickle
 import uuid
 import time
+import zlib
 
 from core.hash import quick_hash
 import core.tracking as tracking
 
 
 SESSION_TIMEOUT = 3600
-    
+
 
 class SessionManager(object):
     def __init__(self, db_connection):
@@ -20,17 +21,21 @@ class SessionManager(object):
         try:
             session_id = session['sid']
             hash = quick_hash(session_id)
-            session_data = self.sql('SELECT data_pickle, last_activity FROM temporary_storage WHERE id = %s', hash)
-            if session_data and session_data[0][1] > time.time() - SESSION_TIMEOUT:
+            data_pickle, compressed, last_activity = self.sql('SELECT data_pickle, compressed, last_activity FROM temporary_storage WHERE id = %s', hash)[0]
+            
+            if last_activity > time.time() - SESSION_TIMEOUT:
+                if compressed:
+                    data_pickle = zlib.decompress(data_pickle)
+                self.data = cPickle.loads(data_pickle)
                 self.hash = hash
-                self.data = cPickle.loads(session_data[0][0])
                 self._new_id = False
                 return self
-            else:
-                raise KeyError
-        except KeyError:
-            self.new()
-            return self
+            
+        except IndexError:
+            pass
+            
+        self.new()
+        return self
         
     def __getitem__(self, item):
         return self.data[item]
@@ -59,7 +64,40 @@ class SessionManager(object):
     def new(self):
         self.regenerate()
         self.data = {}
+        self._track_start()
     
+    def _track_continue(self):
+        """Record a new page visit."""
+        url_id = tracking.get_url_id(self.sql)
+        last_id = self.data.get('url_id', 0)
+        group_id = self.data['group_id']
+        
+        if url_id == last_id:
+            sql = 'UPDATE visit_pages SET refresh_count = refresh_count + 1 WHERE id = %s'
+            self.sql(sql, self.data['visit_id'])
+        else:
+            sql = 'INSERT INTO visit_pages (group_id, url_id, visit_time) VALUES (%s, %s, UNIX_TIMESTAMP(NOW()))'
+            id = self.sql(sql, group_id, url_id)
+            self.data['visit_id'] = id
+        
+        sql = 'UPDATE visit_groups SET last_activity = UNIX_TIMESTAMP(NOW()), pages_unique = pages_unique + {}, pages_total = pages_total + 1 WHERE id = %s'
+        self.sql(sql.format(int(url_id != last_id)), group_id)
+        self.data['url_id'] = url_id
+    
+    def _track_start(self):
+        """Start a tracking session."""
+        ip_id = tracking.get_ip_id(self.sql)
+        ua_id = tracking.get_ua_id(self.sql)
+        language_id = tracking.get_language_id(self.sql)
+        referrer_id = tracking.get_referrer_id(self.sql)
+        account_id = self.data.get('account_id', 0)
+        
+        sql = ('INSERT INTO visit_groups'
+               ' (account_id, ip_id, user_agent_id, referrer_id, language_id, time_started, last_activity)'
+               ' VALUES (%s, %s, %s, %s, %s, UNIX_TIMESTAMP(NOW()), UNIX_TIMESTAMP(NOW()))')
+        group_id = self.sql(sql, account_id, ip_id, ua_id, referrer_id, language_id)
+        self.data['group_id'] = group_id
+
     def regenerate(self):
         while True:
             session_id = uuid.uuid4().hex
@@ -71,22 +109,40 @@ class SessionManager(object):
                 return session_id
     
     def __exit__(self, *args):
+        self._track_continue()
         data = cPickle.dumps(self.data)
+        data_len = len(data)
+        compressed = False
+        
+        #Compress if too large for blob
+        if data_len > 65535:
+            data = zlib.compress(data)
+            compressed = True
+            
         if self._new_id:
-            self.sql('INSERT INTO temporary_storage (id, data_pickle, last_activity) VALUES(%s, %s, UNIX_TIMESTAMP(NOW()))', self.hash, data)
+            self.sql('INSERT INTO temporary_storage (id, data_pickle, data_len, compressed, last_activity) VALUES(%s, %s, %s, %s, UNIX_TIMESTAMP(NOW()))', self.hash, data, data_len, int(compressed))
         else:
-            self.sql('UPDATE temporary_storage SET data_pickle = %s, last_activity = UNIX_TIMESTAMP(NOW()) WHERE id = %s', data, self.hash)
+            self.sql('UPDATE temporary_storage SET data_pickle = %s, data_len = %s, compressed = %s, last_activity = UNIX_TIMESTAMP(NOW()) WHERE id = %s', data, data_len, int(compressed), self.hash)
             
 
-def session_start(mysql, require_login=False):
+def session_start(mysql, require_login=False, require_admin=False):
     def decorator(func):
         @wraps(func)
         def wrapper(*args, **kwargs):
+            url = '{}?{}'.format(url_for(func.__name__, **kwargs), request.query_string)
             with SessionManager(mysql) as session:
+            
+                if require_admin:
+                    if not session.get('admin', False):
+                        session['admin_redirect'] = url
+                        abort(401)
+                        return redirect(url_for('unauthorized'))
+                        
                 if require_login:
                     if not session.get('account_id', 0):
-                        session['login_redirect'] = '{}?{}'.format(url_for(func.__name__, *args, **kwargs), request.query_string)
+                        session['login_redirect'] = url
                         return redirect(url_for('login'))
+                        
                 return func(session=session, *args, **kwargs)
         return wrapper
     return decorator
