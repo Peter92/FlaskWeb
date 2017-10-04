8e0721ef3381fad5609279764131ab701592e38a
Peter <Peter92@users.noreply.github.com>
2017-10-04 08:50:30 +0100

    Clean login attempts

--- core/auto/session.py
+++
@@ -1,8 +0,0 @@
-from __future__ import absolute_import
-import time
-
-from core.session import SESSION_TIMEOUT
-
-
-def remove_old_sessions(db_connection):
-    return db_connection.sql('DELETE FROM temporary_storage WHERE last_activity < {}'.format(time.time() - SESSION_TIMEOUT))
---
+++ core/maintainance/database..py
@@ -0,0 +1,15 @@
+from __future__ import absolute_import
+
+from core.constants import BAN_TIME_IP, BAN_TIME_ACCOUNT
+from core.session import SESSION_TIMEOUT
+
+
+MIN_TIMEOUT = 7200
+
+
+def remove_old_sessions(sql_execute):
+    return sql_execute('DELETE FROM temporary_storage WHERE last_activity < UNIX_TIMESTAMP(NOW()) - {}'.format(max(MIN_TIMEOUT, SESSION_TIMEOUT)))
+    
+    
+def remove_login_attempts(sql_execute):
+    return sql_execute('DELETE FROM login_attempts WHERE attempt_time < UNIX_TIMESTAMP(NOW()) - {}'.format(max(MIN_TIMEOUT, BAN_TIME_IP, BAN_TIME_ACCOUNT)))
