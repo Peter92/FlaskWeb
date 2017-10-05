93820111d5bd5061bd12393c4dc97987460a60c0
Peter <Peter92@users.noreply.github.com>
2017-10-05 16:24:35 +0100

    Handle all error pages

--- __init__.py
+++ __init__.py
@@ -1,5 +1,6 @@
 from __future__ import absolute_import
 from flask import Flask, render_template, request, flash, url_for, escape
+import traceback
 
 from core.database import *
 import core.tracking as tracking
@@ -8,12 +9,13 @@ from core.constants import *
 from core.hash import password_hash, password_check
 from core.decorators import *
 from core.validation import *
+from extensions.flask_compress import Compress
 
 app = Flask(__name__)
+Compress(app)
 mysql = DatabaseConnection(DATABASE_HOST, DATABASE_NAME, DATABASE_USER, DATABASE_PASSWORD)
 
 #Functions below are just for testing different features and are a mess
-
 
 
 @app.route('/')
@@ -27,7 +29,7 @@ def index(session):
         session['count'] = 1
 
     print session['count'], url_for('login', email='what')
-    return str(session.keys())
+    return dict()
 
 
 @app.route('/register', methods=['GET', 'POST'])
@@ -57,11 +59,12 @@ def register(session):
 
 @app.route('/login', methods=['GET', 'POST'])
 @session_start(mysql)
+@csrf_protection
 @set_template('login.html')
 def login(session):
     warnings = []
     errors = []
-    
+    autofocus = 'username'
     try:
         login_redirect = session['login_redirect']
     except KeyError:
@@ -71,8 +74,7 @@ def login(session):
 
         #Don't allow login if page hasn't already been visited (may be a bot)
         if not session.get('allow_login', False):
-            errors.append('Error when logging in.')
-
+            errors.append('There has been an error, please reload the page.'.format(url_for('login')))
         else:
 
             #Validate and figure out if input was email or username
@@ -80,6 +82,8 @@ def login(session):
             valid_username = not validate_username(request.form['username'], ignore_short=True, ignore_long=True)
             if not valid_email and not valid_username:
                 errors.append('Invalid username/email.')
+            else:
+                autofocus = 'password'
             login_email = request.form['username'] if valid_email else None
             login_username = request.form['username'] if valid_username else None
 
@@ -109,7 +113,7 @@ def login(session):
                     session['account_data'] = {}
                     errors += result['errors']
                     warnings += result['warnings']
-            
+    
     else:
         session['allow_login'] = True
         if not session.get('allow_login_redirect', False):
@@ -125,7 +129,7 @@ def login(session):
     except KeyError:
         pass
     
-    return dict(errors=errors, warnings=warnings, login_redirect=login_redirect)
+    return dict(errors=errors, warnings=warnings, login_redirect=login_redirect, autofocus=autofocus)
     
 
 @app.route('/account')
@@ -137,21 +141,43 @@ def account(session):
 @app.route('/test/')
 @app.route('/test/<int:id>')
 @session_start(mysql)
+@set_template('test.html')
 def test(session, id=0):
     print request.endpoint
-    return str(id)
+    return 5
 
 
 @app.route('/admin')
 @session_start(mysql)
 @require_admin
+@set_template('admin.html')
 def admin(session):
-    return 'u r admin'
+    return dict()
 
-#@app.errorhandler(404)
-#@session_start(mysql)
-#def error_404(session):
-#    return render_template('404.html'), 404
+@app.route('/logout')
+@session_start(mysql)
+def logout(session):
+    session['account_data'] = {}
+    return 'logged out'
+
+
+#Handle all error codes
+from werkzeug.exceptions import default_exceptions
+
+def create_error_function(name):
+    def func(error, session):
+        pass
+    func.__name__ = name
+    return func
+
+for code in default_exceptions:
+    func_name = 'error_{}'.format(code)
+    func = create_error_function(func_name)
+
+    func = set_template('error.html', code, mysql)(func)
+    func = session_start(mysql)(func)
+    func = app.errorhandler(code)(func)
+
 
 app.secret_key = APP_SECRET_KEY
 if __name__ == "__main__":
