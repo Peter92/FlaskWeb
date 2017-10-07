a6cda482a1980806464a13e83644995dbe20d0f4
Peter <Peter92@users.noreply.github.com>
2017-10-07 16:40:04 +0100

    Log html status codes and set content type

--- core/decorators.py
+++ core/decorators.py
@@ -8,37 +8,67 @@
 #   def account_page(session):
 #       return dict(account_id = session['account_id'])
 
-from __future__ import absolute_import
+from __future__ import absolute_import, division
 from functools import wraps
 from flask import redirect, url_for, abort, request, make_response, render_template
+from werkzeug.routing import BuildError
+from werkzeug.http import HTTP_STATUS_CODES
+from werkzeug.exceptions import HTTPException
+import traceback
+import time
 
 from core.constants import *
 from core.session import SessionManager
+    
 
-
-def set_template(template):
+def set_template(template, status_code=200, mysql=None):
     def decorator(func):
         @wraps(func)
         def wrapper(*args, **kwargs):
+                
+            #Catch login redirect request from url
+            if request.args.get('login', False):
+                return require_login(func)(*args, **kwargs)
+            
+            #Get result from function
             result = func(*args, **kwargs)
             if result is None:
                 result = {}
             elif not isinstance(result, dict):
                 return result
-                
-            #Catch login redirect request from url
-            if request.args.get('login', False):
-                return require_login(func)(*args, **kwargs)
-                
-            return render_template(template, 
-                                   get_data=request.args, post_data=request.form, session=kwargs['session'],
-                                   page_url=_get_redirect_url(func, _add_queries={'login': '1'}),
-                                   **result)
+            
+            #Deal with status code errors
+            if 'error_format' not in result:
+                result['error_format'] = '{} {}'.format(status_code, HTTP_STATUS_CODES.get(status_code, 'Unknown Status Code'))
+                for i in args:
+                    if isinstance(i, HTTPException):
+                        result['status_description'] = i.description.split('  ')
+                if status_code == 500 and not result.get('status_description', False):
+                    result['traceback'] = traceback.format_exc()
+                    
+            if mysql is not None and status_code // 100 != 2:
+                mysql.command.log_status_code(status_code, kwargs['session']['group_id'])
+            
+            result['debug'] = {}
+            if not PRODUCTION_SERVER:
+                result['debug']['css_nocache'] = '?random={}'.format(int(time.time()))
+            
+            #Render the response
+            response = render_template(template,
+                                       get_data=request.args, post_data=request.form, session=kwargs['session'],
+                                       login_url=_get_redirect_url(func, _add_queries={'login': '1'}),
+                                       **result), status_code
+            return _add_response_headers(response)
         return wrapper
     return decorator
     
 
 def _get_redirect_url(func, _add_queries={}, *args, **kwargs):
+
+    try:
+        redirect_url = url_for(func.__name__, **kwargs)
+    except BuildError:
+        return url_for('login')
 
     #Remove unneeded query values
     ignore = set(['login'])
@@ -51,18 +81,17 @@ def _get_redirect_url(func, _add_queries={}, *args, **kwargs):
     for k, v in _add_queries.iteritems():
         query_list.append('{}={}'.format(k, v))
     
-    if query_list:
-        return '{}?{}'.format(url_for(func.__name__, **kwargs), '&'.join(query_list))
-    else:
-        return url_for(func.__name__, **kwargs)
+    if not query_list:
+        return redirect_url
+    return '{}?{}'.format(redirect_url, '&'.join(query_list))
 
 
-def session_start(mysql, require_login=False, require_admin=False):
+def session_start(mysql):
     def decorator(func):
         @wraps(func)
         def wrapper(*args, **kwargs):
             with SessionManager(mysql) as session:
-                return add_response_headers(func(session=session, *args, **kwargs))
+                return func(session=session, *args, **kwargs)
         return wrapper
     return decorator
 
@@ -71,9 +100,7 @@ def require_admin(func):
     @wraps(func)
     def wrapper(*args, **kwargs):
         if kwargs['session'].get('account_data', {}).get('permission', 0) < PERMISSION_ADMIN:
-            kwargs['session']['admin_redirect'] = _get_redirect_url(func)
-            kwargs['session'].skip = True
-            return add_response_headers(redirect(url_for('unauthorized')))
+            abort(401)
         return func(*args, **kwargs)
     return wrapper
     
@@ -84,14 +111,30 @@ def require_login(func):
         if not kwargs['session'].get('account_data', {}).get('id', 0):
             kwargs['session']['login_redirect'] = _get_redirect_url(func)
             kwargs['session']['allow_login_redirect'] = True
-            kwargs['session'].skip = True
-            return add_response_headers(redirect(url_for('login')))
+            kwargs['session'].exit()
+            return redirect(url_for('login'))
         return func(*args, **kwargs)
     return wrapper
 
+    
+def csrf_protection(func):
+    """Stop cross-site request forgery attacks.
+    Idea modified from http://flask.pocoo.org/snippets/3/
+    """
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        if request.method == "POST":
+            token = kwargs['session'].get('csrf_token', None)
+            kwargs['session'].generate_csrf_token(override=True)
+            if not token or token != request.form.get('csrf_token', None):
+                abort(403)
+        return func(*args, **kwargs)
+    return wrapper
 
-def add_response_headers(output):
+    
+def _add_response_headers(output):
     """Add any custom headers to the page before it is processed."""
+    
     resp = make_response(output)
     resp.headers['X-Frame-Options'] = "DENY"
     resp.headers['X-XSS-Protection'] = 1
@@ -101,3 +144,14 @@ def add_response_headers(output):
         resp.headers['Pragma'] = 'no-cache'
         resp.headers['Expires'] = 0
     return resp
+    
+    
+def set_content_type(content_type):
+    """Change the content type of a page to allow things like rendering images."""
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            result = func(*args, **kwargs)
+            return result, 200, {'Content-Type': content_type}
+        return wrapper
+    return decorator
