71225ce24e04c2977054441a084c214ee229eec7
Peter <Peter92@users.noreply.github.com>
2017-10-07 16:42:46 +0100

    Fixed function that didn't return anything

--- core/hash.py
+++ core/hash.py
@@ -4,9 +4,6 @@ import base64
 import bcrypt
 
 
-BCRYPT_MAX_LENGTH = 72
-
-
 def quick_hash(x):
     hash = hashlib.sha256()
     hash.update(str(x))
@@ -14,8 +11,10 @@ def quick_hash(x):
 
 
 def _reduce_long_password(password):
-    """This is required for bcrypt to work with longer passwords."""
-    if len(password) > BCRYPT_MAX_LENGTH:
+    """This is required for bcrypt to work with longer passwords,
+    as bcrypt only works with passwords under 72 characters.
+    """
+    if len(password) > 72:
         return base64.b64encode(hashlib.sha256(password).digest())
     return password
 
@@ -24,6 +23,7 @@ def password_hash(password, rounds=12):
     hash = bcrypt.hashpw(_reduce_long_password(password.encode('utf-8')), bcrypt.gensalt(rounds=rounds))
     if not password_check(password, hash):
         raise TypeError('failed to hash password')
+    return hash
     
     
 def password_check(password, hash):
